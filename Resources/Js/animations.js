cb356df5c568586028d3aa81fb74e30c462e776d
VeryLowProfile <enrico.gorelli@gmail.com>
2022-03-14 00:29:24 +0100

    added aline animations

--- Resources/Js/animations.js
+++ Resources/Js/animations.js
@@ -24,5 +24,40 @@ const navSlide = () => {
     });
 }
 
+const lineAnimation = () => {
+
+    const skillLines = document.querySelectorAll('.line');
+
+    skillLines.forEach((line) => {
+        line.classList.forEach((cl) => {
+            if (cl === "width_100"){
+                line.style.width = "100%"
+                line.style.animation = `width100 2s ease`
+            }
+            if (cl === "width_90"){
+                line.style.width = "90%"
+                line.style.animation = `width90 2s ease`
+            }
+            if (cl === "width_80"){
+                line.style.width = "80%"
+                line.style.animation = `width80 2s ease`
+            }
+            if (cl === "width_70"){
+                line.style.width = "70%"
+                line.style.animation = `width70 2s ease`
+            }
+            if (cl === "width_60"){
+                line.style.width = "60%"
+                line.style.animation = `width60 2s ease`
+            }
+            if (cl === "width_50"){
+                line.style.width = "50%"
+                line.style.animation = `width50 2s ease`
+            }
+        });
+    });
+}
+
 navSlide();
+lineAnimation();
 
